6964c53d0e8d424bf67159e25a895212b198286f
Manuel Marinez <marinezdev@gmail.com>
2018-10-22 05:39:49 -0400

    Add journal structure file

--- journal.py
+++ journal.py
@@ -1,10 +1,26 @@
+import os
+
+
 def load():
-    return []
+    data = []
+
+    if os.path.exists(get_pathname()):
+        with open(get_pathname()) as file:
+            for entry in file.readlines():
+                data.append(entry.rstrip())
+
+    return data
 
 
 def save(journal_data):
-    pass
+    print('... Saving in {}'.format(get_pathname()))
 
+    with open(get_pathname(), 'w') as fout:
+        for entry in journal_data:
+            fout.write(entry + '\n')
+
+def get_pathname():
+    return os.path.abspath(os.path.join('.' , 'journals', 'TODO' + '.txt'))
 
 def journal_entry():
     pass
